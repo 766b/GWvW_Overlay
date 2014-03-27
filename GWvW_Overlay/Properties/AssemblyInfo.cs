2fd955a3d67a84c1bb1b0908893cade4aa8e139e
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-03-27 13:43:03 +0100

    Detailed version number usage and increased it to reflect the changes made when not adopting this notation

--- GWvW_Overlay/Properties/AssemblyInfo.cs
+++ GWvW_Overlay/Properties/AssemblyInfo.cs
@@ -43,13 +43,13 @@ using System.Windows;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Major Version : incompatible API change
+//      Minor Version : added funtionality in a backward comatible manner
+//      Patch         : backward-compatible bug fixes
+//      
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.6.4")]
-//[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("0.3.7")]
+[assembly: AssemblyFileVersion("0.3.7")]
